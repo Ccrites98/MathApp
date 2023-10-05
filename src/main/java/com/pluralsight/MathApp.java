5a4ca17cc0d4bff594f854dc0ecf5638d0de2770
Christian Crites <ccrites98@gmail.com>
2023-10-05 08:58:39 -0400

    MathApp

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+class MathApp {
+    public static void main(String[] args) {
+        // Question Nombre 1
+        //Bob and Gary's salaries and such
+        double bobSalary = 1000.00;
+        double garySalary = 900.00d;
+        double highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println ("The Highest Salary Is" + highestSalary);
+        //System.out.println () for final product
+        //Question Nombre 2
+        //Carprice and truckprice
+        double CarPrice = 25000.00;
+        double TruckPrice = 50000.00;
+        double LowestPrice = Math.min(CarPrice, TruckPrice);
+        System.out.println("The Lowest Price is" + LowestPrice);
+        //REMEMBER: Check spaces!!!!!!!!!!!
+        //Question Nombre 3
+        //Finding circle area with radius (basically geometry class)
+        int radius;
+        double pi = 3.142, area;
+        radius = (int) 7.25;
+        area = pi * radius * radius;
+        System.out.println("Area of circle is" + area);
+        //I hate math class.
+        //REMEMBER: When calculating circle area, Radius*Pi
+        //Question Nombre 4
+        //Find Square root of 5
+        int A = 5;
+        double R = Math.pow(A, .5);
+        System.out.println("The Square Root of " + A + "is" + R);
+        //I really hate math class.
+        //REMEMBER DECLARED VARIABLES!
+        //Question Nombre 5
+        //
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+    }
